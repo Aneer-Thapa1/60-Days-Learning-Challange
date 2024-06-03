ad5412cdade660331621648e09703ea8ab1461d3
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-06-03 21:10:51 +0545

    day 2 not done

--- main project/myproject/myproject/settings.py
+++ main project/myproject/myproject/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog',  
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
