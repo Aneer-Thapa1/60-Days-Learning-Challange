f64736decde82a655be094bcd764b99b54f2594a
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2024-10-04 19:57:05 +0545

    day 1 memory mosaic

--- Project/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
